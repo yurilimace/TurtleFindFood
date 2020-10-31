7e573161f245603e02cd11a8cd97116371f7022f
José Yuri <yurilima.ce@gmail.com>
2020-10-31 16:07:30 -0300

    implementação do algoritmo A*

---
+++ AstarAlgoritmo.py
@@ -0,0 +1,96 @@
+import numpy
+from random import randint
+import time
+
+def CriandoMatriz(matriz):
+    linha = []
+    for i in range(0,9):
+        for j in range(0,9):
+            if i == 0 and j == 0:
+                linha.append(0)
+            elif i == 5 and j == 5:
+                linha.append(1)
+            else:
+                linha.append(2)
+        matriz.append(linha)
+        linha = []
+
+def PrintaMatriz(matriz):
+    for i in matriz:
+            print(i)
+
+
+
+def aStar():
+    pontoFinal = [0,0]
+    pontoInicial = [0,9]
+    distanciaP = numpy.linalg.norm(numpy.array(pontoInicial)-numpy.array(pontoFinal))
+    caminho = []
+    caminho.append(pontoInicial)
+    loop = True
+    while(loop):
+        distancias = []
+        pontos = []
+
+        #verificar a casa de cima
+        ponto1 = [pontoInicial[0]-1,pontoInicial[1]]
+        if(ponto1[0] >= 0):
+            pontos.append(ponto1)
+            distancias.append(numpy.linalg.norm(numpy.array(ponto1) - numpy.array(pontoFinal)))
+        #verificar  a casa de baixo
+        ponto1 = [pontoInicial[0]+1, pontoInicial[1]]
+        if(ponto1[0] < 9):
+            distancias.append(numpy.linalg.norm(numpy.array(ponto1) - numpy.array(pontoFinal)))
+            pontos.append(ponto1)
+
+        #verificar a casa de direita
+        ponto1 = [pontoInicial[0], pontoInicial[1]+1]
+        if(ponto1[1] < 9):
+            distancias.append(numpy.linalg.norm(numpy.array(ponto1)-numpy.array(pontoFinal)))
+            pontos.append(ponto1)
+        #verificar a casa da esquerda
+        ponto1 = [pontoInicial[0], pontoInicial[1] - 1]
+        if(ponto1[1]>=0):
+            distancias.append(numpy.linalg.norm(numpy.array(ponto1)-numpy.array(pontoFinal)))
+            pontos.append(ponto1)
+        #print(pontos)
+
+        #remove se o caminho estiver dentro de pontos
+        if len(caminho) > 0:
+            for i in caminho:
+                if i in pontos:
+                    index = pontos.index(i)
+                    pontos.remove(i)
+                    del(distancias[index])
+
+
+
+        # index da menor distancia calculada comparada com a distancia P
+        low = distancias.index(min(distancias))
+        caminho.append(pontos[low])
+        pontoInicial = pontos[low]
+        distanciaP = numpy.linalg.norm(numpy.array(pontoInicial) - numpy.array(pontoFinal))
+
+        if(pontoFinal == pontoInicial):
+            print("caminho pronto")
+            print(caminho)
+            loop = False;
+            break;
+
+
+
+
+
+
+
+def main():
+    matriz = []
+    CriandoMatriz(matriz)
+    #PrintaMatriz(matriz)
+    #print(matriz)
+    #pontoA = numpy.array((1,1))
+    #pontoB = numpy.array((10,10))
+    #print(pontoA[0])
+    aStar()
+
+main()
